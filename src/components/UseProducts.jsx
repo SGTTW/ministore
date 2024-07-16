7189c21ab495705a58668e95458dacf879379b1c
SGTTW <88731480+SGTTW@users.noreply.github.com>
2024-07-16 10:25:43 +0100

    fixing http error

--- src/components/UseProducts.jsx
+++ src/components/UseProducts.jsx
@@ -6,6 +6,17 @@ const UseProducts = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    // fetch(
+    //   "/api/products?organization_id=6c3179c6803f48e3876ae866e712175c&reverse_sort=false&page=1&size=10&Appid=R24KSJ5CO7WCO9W&Apikey=c06df05fe6da40f18944772b4c0c2ac920240712160855563654",
+    //   {
+    //     headers: {
+    //       Appid: "R24KSJ5CO7WCO9W",
+    //       Apikey: "c06df05fe6da40f18944772b4c0c2ac920240712160855563654",
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // )
+
     fetch(
       "/api/products?organization_id=6c3179c6803f48e3876ae866e712175c&reverse_sort=false&page=1&size=10&Appid=R24KSJ5CO7WCO9W&Apikey=c06df05fe6da40f18944772b4c0c2ac920240712160855563654",
       {
@@ -16,6 +27,7 @@ const UseProducts = () => {
         },
       }
     )
+      // --------------------
       // .then((res) => {
       //   if (!res.ok) {
       //     throw new Error(`HTTP error! status: ${res.status}`);
@@ -33,7 +45,26 @@ const UseProducts = () => {
       // });
 
       // let's temporary remove the error handling
-      .then((res) => res.json())
+      // .then((res) => res.json())
+      // .then((data) => {
+      //   console.log("API response:", data);
+      //   setProducts(data.items || []);
+      //   setLoading(false);
+      // })
+      // .catch((error) => {
+      //   console.error("Full error:", error);
+      //   setError(error.toString());
+      //   setLoading(false);
+      // });
+
+      // ------- keeping the error logged
+
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
       .then((data) => {
         console.log("API response:", data);
         setProducts(data.items || []);
